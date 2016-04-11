6aab8c185b8a0f8fe0bab49e0f424069854ed794
wlMalk <wm.wexem@gmail.com>
2016-04-11 17:09:54 -0500

    Defined some consts in framework package and new defaults

--- defaults.go
+++ defaults.go
@@ -11,6 +11,8 @@ var (
 
 	Indent = true
 
+	MaxMemory int64 = 32 << 20 // 32 MB
+
 	Schemes = []string{framework.SCHEME_HTTP, framework.SCHEME_HTTPS}
 	Accepts = []string{framework.MIME_JSON}
 	Returns = []string{framework.MIME_JSON}
